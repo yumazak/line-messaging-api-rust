271b908e901777c6d718428c4c6622df0b12eb91
uma <blog3170@gmail.com>
2018-06-10 04:11:35 +0900

    add url test

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,33 @@
+extern crate rustc_serialize as serialize;
+extern crate reqwest;
+extern crate crypto;
+#[macro_use]
+extern crate serde_json;
+
 pub mod line_actions;
-mod structs;
+pub mod line_bot; 
+mod line_messages;
+pub mod structs;
+
+
 
 #[cfg(test)]
 mod tests {
+    use super::line_bot::LineBot;
+    use super::structs::LineBotConfig;
+    use super::line_messages::LineMessage;
+    use super::line_messages::LineMessageType;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn hamc_test() {
+        let bot = LineBot::new("secret key", "this is a pen.");
+        assert_eq!(false, bot.check_signature("a", "This is a pen."));
+    }
+
+    #[test]
+    fn url_test() {
+        let bot = LineBot::new("secret key", "this is a pen.");
+        let message = LineMessage::new(1, LineMessageType::Text, "hi");
+        bot.push("Ua2829b4c5a9b21984c091fc0b641fa8f", vec![message]);
     }
 }
