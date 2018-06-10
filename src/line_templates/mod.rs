7336c8f3895b4f305e6ce5042c5616f906776a6d
uma <blog3170@gmail.com>
2018-06-10 13:22:19 +0900

    add line templates

---
+++ src/line_templates/mod.rs
@@ -0,0 +1,33 @@
+use line_actions::TemplateAction;
+
+#[derive(Serialize, Deserialize)]
+pub enum LineTemplateType {
+    TemplateButtons  { thumbnail_image_url: String, title: String, text: String, actions: Vec<TemplateAction> },
+    TemplateConfirm  { text: String, actions: Vec<TemplateAction> },
+    TemplateCarousel { columns: Vec<TemplateColumn> }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct TemplateComponent {
+    kind: LineTemplateType
+}
+
+impl TemplateComponent {
+    pub fn new (kind: LineTemplateType) -> TemplateComponent {
+        TemplateComponent { kind }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct TemplateColumn {
+    thumbnail_image_url: String,
+    title:               String,
+    text:                String,
+    actions:             Vec<TemplateAction>
+}
+
+impl TemplateColumn {
+    pub fn new (thumbnail_image_url: String, title: String, text: String, actions: Vec<TemplateAction>) -> TemplateColumn {
+        TemplateColumn { thumbnail_image_url, title, text, actions } 
+    }
+}
