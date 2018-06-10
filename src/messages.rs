79f72dcf02d3d7db5dd4380b0e8e6bf2cb895ee7
uma <blog3170@gmail.com>
2018-06-10 23:44:07 +0900

    add File to LineMessageType

--- src/messages.rs
+++ src/messages.rs
@@ -4,22 +4,33 @@ use actions::ImagemapAction;
 use templates::TemplateComponent;
 
 #[derive(Serialize, Deserialize)]
-#[serde(tag = "type", rename_all = "lowercase")]
+#[serde(tag = "type", rename_all = "camelCase")]
 pub enum LineMessageType {
     Text     { text: String },
-    Image    { original_content_url: String, preview_image_url: String },
-    Sticker  { package_id: String, sticker_id: String },
-    Video    { original_content_url: String, preview_image_url: String },
-    Audio    { original_content_url: String, duration: u32 },
-    Location { address: String, latitude: u32, longitude: String },
+    Image    { },
+    Video    { },
+    Audio    { },
+    Location { address: String, latitude: f64, longitude: f64 },
     Imagemap { base_url: String, alt_text: String, base_size: u32, actions: Vec<ImagemapAction> },
-    Template { alt_text: String, template: TemplateComponent }
+    Template { alt_text: String, template: TemplateComponent },
+    File     {
+        #[serde(rename = "fileName")]
+        file_name: String,
+        #[serde(rename = "fileSize")]
+        file_size: String,
+    },
+    Sticker  {
+        #[serde(rename = "packageId")]
+        package_id: String,
+        #[serde(rename = "stickerId")]
+        sticker_id: String
+    },
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct LineMessage {
     id:   String,
-    #[serde(flatten)]
+    #[serde(flatten, rename = "type")]
     kind: LineMessageType,
 }
 
