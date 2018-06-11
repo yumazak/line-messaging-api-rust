514913159d6036d4d48fd5a6d794a1da080bb39a
uma <blog3170@gmail.com>
2018-06-11 23:09:13 +0900

    add to_events method

---
+++ src/utils.rs
@@ -0,0 +1,21 @@
+use serde::de::Deserialize;
+use serde_json;
+use serde_json::Error;
+use serde_json::value::Value;
+
+use events::{ ReplyableEvent };
+
+pub fn to_events<'a, T>(data: &str) -> Result<T, String>
+where for<'de> T: Deserialize<'de>, 
+{
+    let events: Value = match serde_json::from_str(data) {
+        Ok(events) => events,
+        Err(err) => return Err(err.to_string()),
+    };
+    let contents: Value = match serde_json::to_value(&events["events"][0]) {
+        Ok(contents) => contents,
+        Err(err) => return Err(err.to_string()),
+    };
+    
+    serde_json::from_value(contents).map_err(|err| { err.to_string() })
+}
