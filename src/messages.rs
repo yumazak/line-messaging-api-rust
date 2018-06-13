a675a12d649aee97e902c936812e0aa2a2fd836f
uma <blog3170@gmail.com>
2018-06-13 17:17:46 +0900

    refactoring

--- src/messages.rs
+++ src/messages.rs
@@ -75,7 +75,7 @@ pub enum LineMessageType {
 pub struct LineMessage {
     #[serde(flatten, rename = "type")]
     pub kind: LineMessageType,
-    pub id:   String,
+    pub id  : String,
 }
 
 //builder and getter
@@ -99,98 +99,98 @@ impl LineMessage {
         match self.kind.clone() {
             LineMessageType::Template { alt_text, template }                     => Some(alt_text),
             LineMessageType::Imagemap { base_url, alt_text, base_size, actions } => Some(base_url),    
-            _ => None
+            _                                                                    => None
         }
     }
 
     pub fn get_tempate(&self) -> Option<TemplateComponent> {
         match self.kind.clone() {
             LineMessageType::Template { alt_text, template } => Some(template),
-            _ => None
+            _                                                => None
         }
     }
 
     pub fn get_address(&self) -> Option<String> {
         match self.kind.clone() {
             LineMessageType::Location { title, address, latitude, longitude } => Some(address),
-            _ => None
+            _                                                                 => None
         }
     }
 
     pub fn get_latitude(&self) -> Option<f64> {
         match self.kind.clone() {
             LineMessageType::Location { title, address, latitude, longitude } => Some(latitude),
-            _ => None
+            _                                                                 => None
         }
     }
 
     pub fn get_longitude(&self) -> Option<f64> {
         match self.kind.clone() {
             LineMessageType::Location { title, address, latitude, longitude } => Some(longitude),
-            _ => None
+            _                                                                 => None
         }
     }
 
     pub fn get_base_url(&self) -> Option<String> {
         match self.kind.clone() {
             LineMessageType::Imagemap { base_url, alt_text, base_size, actions } => Some(base_url),
-            _ => None
+            _                                                                    => None
         }
     }
 
     pub fn get_base_size(&self) -> Option<BaseSize> {
         match self.kind.clone() {
             LineMessageType::Imagemap { base_url, alt_text, base_size, actions } => Some(base_size),
-            _ => None
+            _                                                                    => None
         }
     }
     
     pub fn get_base_size_height(&self) -> Option<u64> {
         match self.kind.clone() {
             LineMessageType::Imagemap { base_url, alt_text, base_size, actions } => Some(base_size.height),
-            _ => None
+            _                                                                    => None
         }
     }
 
     pub fn get_base_size_width(&self) -> Option<u64> {
         match self.kind.clone() {
             LineMessageType::Imagemap { base_url, alt_text, base_size, actions } => Some(base_size.width),
-            _ => None
+            _                                                                    => None
         }
     }
 
     pub fn get_action(&self) -> Option<Vec<ImagemapAction>> {
         match self.kind.clone() {
             LineMessageType::Imagemap { base_url, alt_text, base_size, actions } => Some(actions),
-            _ => None
+            _                                                                    => None
         }
     }
 
     pub fn get_file_name(&self) -> Option<String> {
         match self.kind.clone() {
             LineMessageType::File { file_name, file_size } => Some(file_name),
-            _ => None
+            _                                              => None
         }
     }
 
     pub fn get_file_size(&self) -> Option<u64> {
         match self.kind.clone() {
             LineMessageType::File { file_name, file_size } => Some(file_size),
-            _ => None
+            _                                              => None
         }
     }
 
     pub fn get_package_id(&self) -> Option<String> {
         match self.kind.clone() {
             LineMessageType::Sticker { package_id, sticker_id } => Some(package_id),
-            _ => None
+            _                                                   => None
         }
     }
 
     pub fn get_sticker_id(&self) -> Option<String> {
         match self.kind.clone() {
             LineMessageType::Sticker { package_id, sticker_id } => Some(sticker_id),
-            _ => None
+            _                                                   => None
         }
     }
 }
